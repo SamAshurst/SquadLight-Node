e9dad32f36a8c7d2ce855f9a1599031f01cdff09
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-03-22 12:16:12 +0000

    Adding functionality to join a room

--- server.js
+++ server.js
@@ -15,11 +15,26 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
     socket.broadcast.emit("message", `${socket.id} connected`);
     // when a user sends a message, broadcast it to all other users
-    socket.on("message", (message) => {
-        socket.broadcast.emit("message", formatMessage(socket.id, message));
-        console.log("message", formatMessage(socket.id, message));
+
+    socket.on("joinRoom", (room) => {
+        const user = { username: socket.id, room };
+        socket.join(user.room);
+
+        socket.broadcast
+            .to(user.room)
+            .emit("message", `${user.username} has joined the chat`);
+
+        socket.on("message", (message) => {
+            io.to(user.room).emit(
+                "message",
+                formatMessage(user.username, message)
+            );
+            io.to(user.room).emit("message", user);
+            // socket.broadcast.emit("message", formatMessage(socket.id, message));
+            // console.log("message", formatMessage(socket.id, message));
+        });
     });
-    // if a user disconnects, log it to the console
+
     socket.on("disconnect", (reason) => {
         socket.broadcast.emit(`User disconnected (${reason})`);
     });
