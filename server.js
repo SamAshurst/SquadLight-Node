89b855103c9a454a57c62f018921aa37874d1bbf
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-03-22 09:41:56 +0000

    Adds formatting to messages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,19 +13,19 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-    console.log("User connected");
     socket.broadcast.emit("message", `${socket.id} connected`);
     // when a user sends a message, broadcast it to all other users
     socket.on("message", (message) => {
-        console.log(`Message: ${message}`);
-        socket.broadcast.emit("message", message);
+        socket.broadcast.emit("message", formatMessage(socket.id, message));
+        console.log("message", formatMessage(socket.id, message));
     });
     // if a user disconnects, log it to the console
     socket.on("disconnect", (reason) => {
-        console.log(`User disconnected (${reason})`);
+        socket.broadcast.emit(`User disconnected (${reason})`);
     });
 });
 
 server.listen(3000, () => {
+    // eslint-disable-next-line no-console
     console.log("listening on *:3000");
 });
